c55c73b6c95834d285f41059ba5e0c0040375dd5
henrykorir <longkhery@gmail.com>
2023-05-26 14:43:25 -0700

    Added Happy Betty styled code

---
+++ shell_0.c
@@ -0,0 +1,13 @@
+/**
+ * main - Entry point
+ * @argc: number of commandline
+ * @argv: array of commands
+ * @envp: pointer to the environment
+ * Return: Always 0 (success)
+ */
+int main(int argc,
+char **argv,
+char **envp)
+{
+	return (0);
+}
